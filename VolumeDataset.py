3a04a2fa91d48e30e88bbb707a38af2b66281767
Zhou <yue.zhou@tum.de>
2020-12-03 20:09:23 +0100

    modified on encoder and decoder

---
+++ VolumeDataset.py
@@ -0,0 +1,183 @@
+import os.path
+import random
+import torchvision.transforms as transforms
+import torch
+from data.base_dataset import BaseDataset
+# from data.image_folder import make_dataset
+import pickle
+import numpy as np
+import SimpleITK as sitk
+import torchio
+from torchio.transforms import (
+    RescaleIntensity,
+    RandomAffine,
+    RandomElasticDeformation,
+    Compose,
+    OneOf,
+    Crop,
+    Resample,
+    Pad,
+    RandomFlip,
+    CropOrPad,
+    ZNormalization,
+    Lambda
+)
+import napari
+
+
+def load_image_file(path: str) -> np.ndarray:
+    img = sitk.ReadImage(path)
+    return img
+
+
+class VolumeDataset(BaseDataset):
+
+    @staticmethod
+    def modify_commandline_options(parser, is_train):
+        """Add new dataset-specific options, and rewrite default values for existing options.
+
+        Parameters:
+            parser          -- original option parser
+            is_train (bool) -- whether training phase or test phase. You can use this flag to add training-specific or test-specific options.
+
+        Returns:
+            the modified parser.
+
+        """
+        parser.add_argument('--visualize_volume', type=bool, default=False, help='Set visualize to False. it\'s only '
+                                                                                 'used for debugging.')
+        parser.add_argument('--load_mask', type=bool, default=False, help='load prostate mask for seg. loss')
+        parser.add_argument('--inshape', type=int, nargs='+', default=[128] * 3,
+                            help='after cropping shape of input. '
+                                 'default is equal to image size. specify if the input can\'t path through UNet')
+        parser.add_argument('--origshape', type=int, nargs='+', default=[80] * 3,
+                            help='original shape of input images')
+        parser.add_argument('--min_size', type=int, default=80, help='minimum length of the axes')
+        return parser
+
+    def __init__(self, opt):
+        """Initialize this dataset class.
+
+        Parameters:
+            opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
+        """
+        BaseDataset.__init__(self, opt)
+        self.opt = opt
+        self.root = opt.dataroot
+        self.load_mask = opt.load_mask
+
+        self.patients = self.read_list_of_patients()
+        random.shuffle(self.patients)
+        self.subjects = {}
+        # self.mr = {}
+        # self.trus = {}
+
+        assert (self.opt.load_size >= self.opt.crop_size)  # crop_size should be smaller than the size of loaded image
+        self.input_nc = self.opt.output_nc if self.opt.direction == 'BtoA' else self.opt.input_nc
+        self.output_nc = self.opt.input_nc if self.opt.direction == 'BtoA' else self.opt.output_nc
+
+        self.input_size = opt.inshape
+        self.min_size = opt.min_size
+
+        self.transform = self.create_transforms()
+
+        self.means = []
+        self.std = []
+
+    @staticmethod
+    def clip_image(x: torch.Tensor):
+        [l, h] = np.quantile(x.cpu().numpy(), [0.02, 0.98])
+        x[x < l] = l
+        x[x > h] = h
+        return x
+
+    def create_transforms(self):
+        transforms = []
+
+        # clipping to remove outliers (if any)
+        # clip_intensity = Lambda(VolumeDataset.clip_image, types_to_apply=[torchio.INTENSITY])
+        # transforms.append(clip_intensity)
+
+        rescale = RescaleIntensity((-1, 1), percentiles=(0.5, 99.5))
+        # normalize with mu = 0 and sigma = 1/3 to have data in -1...1 almost
+        # ZNormalization()
+
+        transforms.append(rescale)
+
+        # transforms = [rescale]
+        # # As RandomAffine is faster then RandomElasticDeformation, we choose to
+        # # apply RandomAffine 80% of the times and RandomElasticDeformation the rest
+        # # Also, there is a 25% chance that none of them will be applied
+        # if self.opt.isTrain:
+        #     spatial = OneOf(
+        #         {RandomAffine(translation=5): 0.8, RandomElasticDeformation(): 0.2},
+        #         p=0.75,
+        #     )
+        #     transforms += [RandomFlip(axes=(0, 2), p=0.8), spatial]
+
+        self.ratio = self.min_size / np.max(self.input_size)
+        transforms.append(Resample(self.ratio))
+        transforms.append(CropOrPad(self.input_size))
+        transform = Compose(transforms)
+        return transform
+
+    def reverse_resample(self, min_value=-1):
+        transforms = [Resample(1 / self.ratio)]
+        return Compose(transforms + [CropOrPad(self.opt.origshape, padding_mode=min_value)])
+
+    def read_list_of_patients(self):
+        patients = []
+        for root, dirs, files in os.walk(self.root):
+            if ('nonrigid' in root) or ('cropped' not in root):
+                continue
+            if 'trus.mhd' not in files:
+                continue
+            patients.append(root)
+        return patients
+
+    def __getitem__(self, index):
+        sample, subject = self.load_subject_(index)
+        transformed_ = self.transform(subject)
+
+        if self.opt.visualize_volume:
+            with napari.gui_qt():
+                napari.view_image(np.stack([transformed_['mr'].data.squeeze().numpy(),
+                                            transformed_['trus'].data.squeeze().numpy()]))
+
+        dict_ = {
+            'A': transformed_['mr'].data[:, :self.input_size[0], :self.input_size[1], :self.input_size[2]],
+            'B': transformed_['trus'].data[:, :self.input_size[0], :self.input_size[1], :self.input_size[2]],
+            #'Patient': sample.split('/')[-4].replace(' ', ''),
+            'A_paths': sample + "/mr.mhd",
+            'B_paths': sample + "/trus.mhd"
+        }
+        if self.load_mask:
+            dict_['A_mask'] = transformed_['mr_tree'].data[:, :self.input_size[0], :self.input_size[1], :self.input_size[2]]
+
+        return dict_
+
+    def load_subject_(self, index):
+        sample = self.patients[index % len(self.patients)]
+
+        # load mr and turs file if it hasn't already been loaded
+        if sample not in self.subjects:
+            # print(f'loading patient {sample}')
+            if self.load_mask:
+                subject = torchio.Subject(mr=torchio.ScalarImage(sample + "/mr.mhd"),
+                                          trus=torchio.ScalarImage(sample + "/trus.mhd"),
+                                          mr_tree=torchio.LabelMap(sample + "/mr_tree.mhd"))
+            else:
+                subject = torchio.Subject(mr=torchio.ScalarImage(sample + "/mr.mhd"),
+                                          trus=torchio.Image(sample + "/trus.mhd"))
+            self.subjects[sample] = subject
+        subject = self.subjects[sample]
+        return sample, subject
+
+    def __len__(self):
+        if self.opt.isTrain:
+            return len(self.patients)
+        else:
+            return len(self.patients)
+
+    def name(self):
+        return 'VolumeDataset'
---
+++ networks3d.py
@@ -0,0 +1,445 @@
+from torch import nn
+from torch.autograd import Variable
+import torch
+import torch.nn.functional as F
+try:
+    from itertools import izip as zip
+except ImportError: # will be 3.x series
+    pass
+
+##################################################################################
+# Encoder and Decoders
+##################################################################################
+
+class StyleEncoder(nn.Module):
+    def __init__(self, n_downsample, input_dim, dim, style_dim, norm, activ, pad_type):
+        super(StyleEncoder, self).__init__()
+        self.model = []
+        self.model += [Conv3dBlock(input_dim, dim, 7, 1, 3, norm=norm, activation=activ, pad_type=pad_type)]
+        for i in range(2):
+            self.model += [Conv3dBlock(dim, 2 * dim, 4, 2, 1, norm=norm, activation=activ, pad_type=pad_type)]
+            dim *= 2
+        for i in range(n_downsample - 2):
+            self.model += [Conv3dBlock(dim, dim, 4, 2, 1, norm=norm, activation=activ, pad_type=pad_type)]
+        self.model += [nn.AdaptiveAvgPool3d(1)] # global average pooling
+        self.model += [nn.Conv3d(dim, style_dim, 1, 1, 0)]
+        self.model = nn.Sequential(*self.model)
+        self.output_dim = dim
+
+    def forward(self, x):
+        return self.model(x)
+
+class ContentEncoder(nn.Module):
+    def __init__(self, n_downsample, n_res, input_dim, dim, norm, activ, pad_type):
+        super(ContentEncoder, self).__init__()
+        self.model = []
+        self.model += [Conv3dBlock(input_dim, dim, 7, 1, 3, norm=norm, activation=activ, pad_type=pad_type)]
+        # downsampling blocks
+        for i in range(n_downsample):
+            self.model += [Conv3dBlock(dim, 2 * dim, 4, 2, 1, norm=norm, activation=activ, pad_type=pad_type)]
+            dim *= 2
+        # residual blocks
+        self.model += [ResBlocks(n_res, dim, norm=norm, activation=activ, pad_type=pad_type)]
+        self.model = nn.Sequential(*self.model)
+        self.output_dim = dim
+
+    def forward(self, x):
+        return self.model(x)
+
+class Decoder(nn.Module):
+    def __init__(self, n_upsample, n_res, dim, output_dim, res_norm='adain', activ='relu', pad_type='zero'):
+        super(Decoder, self).__init__()
+
+        self.model = []
+        # AdaIN residual blocks
+        self.model += [ResBlocks(n_res, dim, res_norm, activ, pad_type=pad_type)]
+        # upsampling blocks
+        for i in range(n_upsample):
+            self.model += [nn.Upsample(scale_factor=2),
+                           Conv3dBlock(dim, dim // 2, 5, 1, 2, norm='ln', activation=activ, pad_type=pad_type)]
+            dim //= 2
+        # use reflection padding in the last conv layer
+        self.model += [Conv3dBlock(dim, output_dim, 7, 1, 3, norm='none', activation='tanh', pad_type=pad_type)]
+        self.model = nn.Sequential(*self.model)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+##################################################################################
+# Generator
+##################################################################################
+class Generator(nn.Module):
+	pass
+
+
+##################################################################################
+# Discriminator
+##################################################################################
+class Discriminator(nn.Module):
+	pass
+
+
+
+
+
+##################################################################################
+# Sequential Models
+##################################################################################
+
+#######  May Need to modify to 3D ########
+class ResBlocks(nn.Module):
+    def __init__(self, num_blocks, dim, norm='in', activation='relu', pad_type='zero'):
+        super(ResBlocks, self).__init__()
+        self.model = []
+        for i in range(num_blocks):
+            self.model += [ResBlock(dim, norm=norm, activation=activation, pad_type=pad_type)]
+        self.model = nn.Sequential(*self.model)
+
+    def forward(self, x):
+        return self.model(x)
+
+####### May Need to modify to 3D ########
+class MLP(nn.Module):
+    def __init__(self, input_dim, output_dim, dim, n_blk, norm='none', activ='relu'):
+
+        super(MLP, self).__init__()
+        self.model = []
+        self.model += [LinearBlock(input_dim, dim, norm=norm, activation=activ)]
+        for i in range(n_blk - 2):
+            self.model += [LinearBlock(dim, dim, norm=norm, activation=activ)]
+        self.model += [LinearBlock(dim, output_dim, norm='none', activation='none')] # no output activations
+        self.model = nn.Sequential(*self.model)
+
+    def forward(self, x):
+        return self.model(x.view(x.size(0), -1))
+
+
+##################################################################################
+# Basic Blocks
+##################################################################################
+
+####### Do not need to modify to 3D as long as Conv2dBlock is correctly changed########
+class ResBlock(nn.Module):
+    def __init__(self, dim, norm='in', activation='relu', pad_type='replicate'):
+        super(ResBlock, self).__init__()        
+        model = []
+        model += [Conv3dBlock(dim ,dim, 3, 1, 1, norm=norm, activation=activation, pad_type=pad_type)]
+        model += [Conv3dBlock(dim ,dim, 3, 1, 1, norm=norm, activation='none', pad_type=pad_type)]
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        residual = x
+        out = self.model(x)
+        out += residual
+        return out
+
+
+####### !!!Need to modify to 3D ########
+class Conv3dBlock(nn.Module):
+    def __init__(self, input_dim ,output_dim, kernel_size, stride,
+                 padding=0, norm='in', activation='relu', pad_type='zero'):
+        super(Conv3dBlock, self).__init__()
+        self.use_bias = True
+        # initialize padding
+
+        #############Pad Type no function for 3D, reflectionpad and zeropad #########
+        if pad_type == 'reflect':
+            self.pad = nn.ReflectionPad3d(padding)
+        elif pad_type == 'replicate':
+            self.pad = nn.ReplicationPad3d(padding)
+        elif pad_type == 'zero':
+            self.pad = nn.ZeroPad3d(padding)
+        else:
+            assert 0, "Unsupported padding type: {}".format(pad_type)
+
+        # initialize normalization
+        norm_dim = output_dim
+        if norm == 'bn':
+            self.norm = nn.BatchNorm3d(norm_dim)
+        elif norm == 'in':
+            #self.norm = nn.InstanceNorm2d(norm_dim, track_running_stats=True)
+            self.norm = nn.InstanceNorm3d(norm_dim)
+        elif norm == 'ln':
+            self.norm = LayerNorm(norm_dim)
+        elif norm == 'adain':
+            self.norm = AdaptiveInstanceNorm3d(norm_dim)
+        elif norm == 'none' or norm == 'sn':
+            self.norm = None
+        else:
+            assert 0, "Unsupported normalization: {}".format(norm)
+
+        # initialize activation
+        if activation == 'relu':
+            self.activation = nn.ReLU(inplace=True)
+        elif activation == 'lrelu':
+            self.activation = nn.LeakyReLU(0.2, inplace=True)
+        elif activation == 'prelu':
+            self.activation = nn.PReLU()
+        elif activation == 'selu':
+            self.activation = nn.SELU(inplace=True)
+        elif activation == 'tanh':
+            self.activation = nn.Tanh()
+        elif activation == 'none':
+            self.activation = None
+        else:
+            assert 0, "Unsupported activation: {}".format(activation)
+
+        # initialize convolution
+        if norm == 'sn':
+            self.conv = SpectralNorm(nn.Conv3d(input_dim, output_dim, kernel_size, stride, bias=self.use_bias))
+        else:
+            self.conv = nn.Conv3d(input_dim, output_dim, kernel_size, stride, bias=self.use_bias)
+
+    def forward(self, x):
+        x = self.conv(self.pad(x))
+        if self.norm:
+            x = self.norm(x)
+        if self.activation:
+            x = self.activation(x)
+        return x
+
+
+
+####### Not sure if need to modify to 3D, may have to check a bit how the normalization works ########
+class LinearBlock(nn.Module):
+    def __init__(self, input_dim, output_dim, norm='none', activation='relu'):
+        super(LinearBlock, self).__init__()
+        use_bias = True
+        # initialize fully connected layer
+        if norm == 'sn':
+            self.fc = SpectralNorm(nn.Linear(input_dim, output_dim, bias=use_bias))
+        else:
+            self.fc = nn.Linear(input_dim, output_dim, bias=use_bias)
+
+        # initialize normalization
+        norm_dim = output_dim
+        if norm == 'bn':
+            self.norm = nn.BatchNorm1d(norm_dim)
+        elif norm == 'in':
+            self.norm = nn.InstanceNorm1d(norm_dim)
+        elif norm == 'ln':
+            self.norm = LayerNorm(norm_dim)
+        elif norm == 'none' or norm == 'sn':
+            self.norm = None
+        else:
+            assert 0, "Unsupported normalization: {}".format(norm)
+
+        # initialize activation
+        if activation == 'relu':
+            self.activation = nn.ReLU(inplace=True)
+        elif activation == 'lrelu':
+            self.activation = nn.LeakyReLU(0.2, inplace=True)
+        elif activation == 'prelu':
+            self.activation = nn.PReLU()
+        elif activation == 'selu':
+            self.activation = nn.SELU(inplace=True)
+        elif activation == 'tanh':
+            self.activation = nn.Tanh()
+        elif activation == 'none':
+            self.activation = None
+        else:
+            assert 0, "Unsupported activation: {}".format(activation)
+
+    def forward(self, x):
+        out = self.fc(x)
+        if self.norm:
+            out = self.norm(out)
+        if self.activation:
+            out = self.activation(out)
+        return out
+
+
+##################################################################################
+# Normalization layers
+##################################################################################
+
+####### !!!Need to modify to 3D ########
+class AdaptiveInstanceNorm3d(nn.Module):
+    def __init__(self, num_features, eps=1e-5, momentum=0.1):
+        super(AdaptiveInstanceNorm3d, self).__init__()
+        self.num_features = num_features
+        self.eps = eps
+        self.momentum = momentum
+        # weight and bias are dynamically assigned
+        self.weight = None
+        self.bias = None
+        # just dummy buffers, not used
+        self.register_buffer('running_mean', torch.zeros(num_features))
+        self.register_buffer('running_var', torch.ones(num_features))
+
+    def forward(self, x):
+        assert self.weight is not None and self.bias is not None, "Please assign weight and bias before calling AdaIN!"
+        b, c = x.size(0), x.size(1)
+        running_mean = self.running_mean.repeat(b)
+        running_var = self.running_var.repeat(b)
+
+        # Apply instance norm
+        x_reshaped = x.contiguous().view(1, b * c, *x.size()[2:])
+
+        out = F.batch_norm(
+            x_reshaped, running_mean, running_var, self.weight, self.bias,
+            True, self.momentum, self.eps)
+
+        return out.view(b, c, *x.size()[2:])
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' + str(self.num_features) + ')'
+
+
+
+####### Need to check more ########
+class LayerNorm(nn.Module):
+    def __init__(self, num_features, eps=1e-5, affine=True):
+        super(LayerNorm, self).__init__()
+        self.num_features = num_features
+        self.affine = affine
+        self.eps = eps
+
+        if self.affine:
+            self.gamma = nn.Parameter(torch.Tensor(num_features).uniform_())
+            self.beta = nn.Parameter(torch.zeros(num_features))
+
+    def forward(self, x):
+        shape = [-1] + [1] * (x.dim() - 1)
+        # print(x.size())
+        if x.size(0) == 1:
+            # These two lines run much faster in pytorch 0.4 than the two lines listed below.
+            mean = x.view(-1).mean().view(*shape)
+            std = x.view(-1).std().view(*shape)
+        else:
+            mean = x.view(x.size(0), -1).mean(1).view(*shape)
+            std = x.view(x.size(0), -1).std(1).view(*shape)
+
+        x = (x - mean) / (std + self.eps)
+
+        if self.affine:
+            shape = [1, -1] + [1] * (x.dim() - 2)
+            x = x * self.gamma.view(*shape) + self.beta.view(*shape)
+        return x
+
+####### Do not need to modify to 3D ########
+def l2normalize(v, eps=1e-12):
+    return v / (v.norm() + eps)
+
+
+
+####### Need to check a bit more ########
+class SpectralNorm(nn.Module):
+    """
+    Based on the paper "Spectral Normalization for Generative Adversarial Networks" by Takeru Miyato, Toshiki Kataoka, Masanori Koyama, Yuichi Yoshida
+    and the Pytorch implementation https://github.com/christiancosgrove/pytorch-spectral-normalization-gan
+    """
+    def __init__(self, module, name='weight', power_iterations=1):
+        super(SpectralNorm, self).__init__()
+        self.module = module
+        self.name = name
+        self.power_iterations = power_iterations
+        if not self._made_params():
+            self._make_params()
+
+    def _update_u_v(self):
+        u = getattr(self.module, self.name + "_u")
+        v = getattr(self.module, self.name + "_v")
+        w = getattr(self.module, self.name + "_bar")
+
+        height = w.data.shape[0]
+        for _ in range(self.power_iterations):
+            v.data = l2normalize(torch.mv(torch.t(w.view(height,-1).data), u.data))
+            u.data = l2normalize(torch.mv(w.view(height,-1).data, v.data))
+
+        # sigma = torch.dot(u.data, torch.mv(w.view(height,-1).data, v.data))
+        sigma = u.dot(w.view(height, -1).mv(v))
+        setattr(self.module, self.name, w / sigma.expand_as(w))
+
+    def _made_params(self):
+        try:
+            u = getattr(self.module, self.name + "_u")
+            v = getattr(self.module, self.name + "_v")
+            w = getattr(self.module, self.name + "_bar")
+            return True
+        except AttributeError:
+            return False
+
+
+    def _make_params(self):
+        w = getattr(self.module, self.name)
+
+        height = w.data.shape[0]
+        width = w.view(height, -1).data.shape[1]
+
+        u = nn.Parameter(w.data.new(height).normal_(0, 1), requires_grad=False)
+        v = nn.Parameter(w.data.new(width).normal_(0, 1), requires_grad=False)
+        u.data = l2normalize(u.data)
+        v.data = l2normalize(v.data)
+        w_bar = nn.Parameter(w.data)
+
+        del self.module._parameters[self.name]
+
+        self.module.register_parameter(self.name + "_u", u)
+        self.module.register_parameter(self.name + "_v", v)
+        self.module.register_parameter(self.name + "_bar", w_bar)
+
+
+    def forward(self, *args):
+        self._update_u_v()
+        return self.module.forward(*args)
+
+##################################################################################
+# VGG network definition
+##################################################################################
+
+####### !!!Need to modify to 3D, but in the whole pipeline, I didn't fine this class Vgg16 #######
+####### in the network, what they used is to load a pretrained VGG net, and add a vgg_loss #######
+####### when traing, but we do not have a vgg_pretrained_model for 3D, do we??             #######                   
+
+class Vgg16(nn.Module):
+    def __init__(self):
+        super(Vgg16, self).__init__()
+        self.conv1_1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.conv1_2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+
+        self.conv2_1 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv2_2 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+
+        self.conv3_1 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.conv3_2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.conv3_3 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+
+        self.conv4_1 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
+        self.conv4_2 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.conv4_3 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+
+        self.conv5_1 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.conv5_2 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.conv5_3 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, X):
+        h = F.relu(self.conv1_1(X), inplace=True)
+        h = F.relu(self.conv1_2(h), inplace=True)
+        # relu1_2 = h
+        h = F.max_pool2d(h, kernel_size=2, stride=2)
+
+        h = F.relu(self.conv2_1(h), inplace=True)
+        h = F.relu(self.conv2_2(h), inplace=True)
+        # relu2_2 = h
+        h = F.max_pool2d(h, kernel_size=2, stride=2)
+
+        h = F.relu(self.conv3_1(h), inplace=True)
+        h = F.relu(self.conv3_2(h), inplace=True)
+        h = F.relu(self.conv3_3(h), inplace=True)
+        # relu3_3 = h
+        h = F.max_pool2d(h, kernel_size=2, stride=2)
+
+        h = F.relu(self.conv4_1(h), inplace=True)
+        h = F.relu(self.conv4_2(h), inplace=True)
+        h = F.relu(self.conv4_3(h), inplace=True)
+        # relu4_3 = h
+
+        h = F.relu(self.conv5_1(h), inplace=True)
+        h = F.relu(self.conv5_2(h), inplace=True)
+        h = F.relu(self.conv5_3(h), inplace=True)
+        relu5_3 = h
+
+        return relu5_3
+        # return [relu1_2, relu2_2, relu3_3, relu4_3]
